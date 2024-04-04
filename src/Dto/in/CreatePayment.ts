b9ddfa9881028a7d09020db488d1b7004d2913d9
Campione <80720402+alfrecampione@users.noreply.github.com>
2024-04-03 22:49:26 -0400

    activity 2

---
+++ src/Dto/in/CreatePayment.ts
@@ -0,0 +1,6 @@
+export interface CreatePaymentDto{
+    Amount : number
+    Period : number
+    PayDate : string
+    Status : string
+}
---
+++ src/Dto/in/CreatePerson.ts
@@ -0,0 +1,9 @@
+export interface CreatePersonDto{
+    CI : string
+    FullName : string
+    Email : string
+    InterestRate : number
+    StartAmount : number
+    PaidAmount : number
+    TotalAmount : number
+}
